58370baf912d4fe1bdd5cf4e85daa057b8436541
ScottHensen <hensen.scott@gmail.com>
2018-11-19 20:35:58 -0700

    initial commit - blank project

---
+++ src/main/java/life/toodoo/svc/ToodooSvcApplication.java
@@ -0,0 +1,12 @@
+package life.toodoo.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToodooSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToodooSvcApplication.class, args);
+	}
+}
